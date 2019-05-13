7fb71d560cf70e580dc5bb5829dd5254f029c61a
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2019-05-13 10:39:19 -0700

    Add support for pretty-printing non-http log messages
    By passing "-a" (for "all") non-http log messages will be passed through
    to pino-pretty. That way we can support both http and non-http logs with
    a single tool.

--- cmd.js
+++ cmd.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
-var printer = require('./')()
+const args = require('args')
+const printFactory = require('./')
 
-process.stdin.pipe(printer)
+args
+  .option(['a', 'all'], 'Handle all log messages, not just HTTP request/response logs')
+
+args
+  .example('cat log | pino-http-print', 'To prettify only HTTP logs, simply pipe a log file through')
+  .example('cat log | pino-http-print -a', 'To prettify both HTTP and non-HTTP log messages use the -a option')
+
+const opts = args.parse(process.argv)
+const printer = printFactory(opts)
+
+process.stdin.pipe(printer())

af1fc07746b7ce354f9c2bfd26826ef1dc251c01
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-09-29 15:59:22 +0200

    chore: update test;

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const printerFactory = require('./')
+const printerFactory = require('../')
 const printer = printerFactory()
 const through = require('through2')
-const test = require('tap').test
+const { test } = require('tap')
 const strip = require('strip-ansi')
 
 const log = '{"pid":13961,"hostname":"MacBook-Pro-4","level":30,"time":1469122492244,"msg":"request completed","res":{"statusCode":200,"header":"HTTP/1.1 200 OK\\r\\ncontent-type: application/json; charset=utf-8\\r\\ncache-control: no-cache\\r\\nvary: accept-encoding\\r\\ncontent-encoding: gzip\\r\\ndate: Thu, 21 Jul 2016 17:34:52 GMT\\r\\nconnection: close\\r\\ntransfer-encoding: chunked\\r\\n\\r\\n"},"responseTime":17,"req":{"id":8,"method":"GET","url":"/api/activity/component","headers":{"host":"localhost:20000","connection":"keep-alive","cache-control":"max-age=0","accept":"application/json","user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36","referer":"http://localhost:20000/","accept-encoding":"gzip, deflate, sdch","accept-language":"en-US,en;q=0.8,de;q=0.6","cookie":"_ga=GA1.1.204420087.1444842476"},"remoteAddress":"127.0.0.1","remotePort":61543},"v":1}\n'
@@ -13,7 +13,7 @@ const nonJSONLog = 'this is just some raw text that can\'t parse as JSON\n'
 test('outputs log message for req/res serialized pino log', function (assert) {
   const expected = '[1469122492244] GET http://localhost:20000/api/activity/component 200 17ms\n'
   const p = printer(through(function (line) {
-    assert.is(strip(line.toString()), expected)
+    assert.equal(strip(line.toString()), expected)
     assert.end()
   }))
   p.write(log)
@@ -23,7 +23,7 @@ test('translates time when option is set', function (assert) {
   const translateTimePrinter = printerFactory({ translateTime: true })
   const expected = '[2016-07-21 17:34:52.244 +0000] GET http://localhost:20000/api/activity/component 200 17ms\n'
   const p = translateTimePrinter(through(function (line) {
-    assert.is(strip(line.toString()), expected)
+    assert.equal(strip(line.toString()), expected)
     assert.end()
   }))
   p.write(log)
@@ -33,7 +33,7 @@ test('use relative url when option is set', function (assert) {
   const relativeUrlPrinter = printerFactory({ relativeUrl: true })
   const expected = '[1469122492244] GET /api/activity/component 200 17ms\n'
   const p = relativeUrlPrinter(through(function (line) {
-    assert.is(strip(line.toString()), expected)
+    assert.equal(strip(line.toString()), expected)
     assert.end()
   }))
   p.write(log)
@@ -43,7 +43,7 @@ test('colorize when option is set (http log)', function (assert) {
   const coloredPrinter = printerFactory({ colorize: true })
   const expected = '[1469122492244] \u001B[36mGET\u001B[39m http://localhost:20000/api/activity/component \u001B[32m200\u001B[39m 17ms\n'
   const p = coloredPrinter(through(function (line) {
-    assert.is(line.toString(), expected)
+    assert.equal(line.toString(), expected)
     assert.end()
   }))
   p.write(log)
@@ -53,7 +53,7 @@ test('colorize when option is set (non-http log)', function (assert) {
   const coloredPrinter = printerFactory({ colorize: true, all: true })
   const expected = '[1557721475837] \u001B[32mINFO \u001B[39m (48079 on MacBook-Pro-4): \u001B[36mThis is not a request/response log\u001B[39m\n'
   const p = coloredPrinter(through(function (line) {
-    assert.is(line.toString(), expected)
+    assert.equal(line.toString(), expected)
     assert.end()
   }))
   p.write(nonHttpLog)
@@ -69,7 +69,7 @@ test('does not output non-http log messages by default', function (assert) {
   p.write(nonHttpLog)
 
   setImmediate(() => {
-    assert.is(printedLines.length, 0)
+    assert.equal(printedLines.length, 0)
     assert.end()
   })
 })
@@ -87,8 +87,8 @@ test('outputs non-http log messages when `all` option is set to `true`', functio
   p.write(nonHttpLog)
 
   setImmediate(() => {
-    assert.is(printedLines.length, 1)
-    assert.is(printedLines[0], expected)
+    assert.equal(printedLines.length, 1)
+    assert.equal(printedLines[0], expected)
     assert.end()
   })
 })
@@ -106,8 +106,8 @@ test('passes options to pino-pretty when `all` option is set to `true`', functio
   p.write(nonHttpLog)
 
   setImmediate(() => {
-    assert.is(printedLines.length, 1)
-    assert.is(printedLines[0], expected)
+    assert.equal(printedLines.length, 1)
+    assert.equal(printedLines[0], expected)
     assert.end()
   })
 })
@@ -118,7 +118,7 @@ test('logs to process.stdout by default', function (assert) {
   const write = process.stdout.write
   process.stdout.write = function (chunk, enc, cb) {
     process.stdout.write = write
-    assert.is(strip(chunk.toString()), expected)
+    assert.equal(strip(chunk.toString()), expected)
     assert.end()
   }
   p.write(log)
@@ -136,7 +136,7 @@ test('disables parser strictness and discards invalid lines when the `lax` optio
   p.write(nonJSONLog)
 
   setImmediate(() => {
-    assert.is(printedLines.length, 0)
+    assert.equal(printedLines.length, 0)
     assert.end()
   })
 })
